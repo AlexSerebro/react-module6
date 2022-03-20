8994342592ae9284d28c0218649aec0d586ae568
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-03-20 19:05:00 +0200

    add static

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,6 +2,13 @@ import React from "react";
 import s from "./Counter.module.css";
 
 class Counter extends React.Component {
+  static defaultProps = {
+    initialValue: 0,
+  };
+
+  static propTypes = {
+    //
+  };
   //
   // constructor() {
   //   super();
@@ -11,7 +18,7 @@ class Counter extends React.Component {
   //   };
   // }
   // Все що вище це аналог state
-  state = { value: 0 };
+  state = { value: this.props.initialValue };
 
   handleIncrement = () => {
     this.setState((prevState) => {
@@ -21,11 +28,9 @@ class Counter extends React.Component {
     });
   };
   handleDicrement = () => {
-    this.setState((prevState) => {
-      return {
-        value: prevState.value - 1,
-      };
-    });
+    this.setState((prevState) => ({
+      value: prevState.value - 1,
+    }));
   };
   render() {
     return (
