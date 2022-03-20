84d5f85c4d77899f56b4423d336ea07b88c77f06
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-03-20 18:53:43 +0200

    add counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,13 +2,42 @@ import React from "react";
 import s from "./Counter.module.css";
 
 class Counter extends React.Component {
+  //
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     value: 0,
+  //   };
+  // }
+  // Все що вище це аналог state
+  state = { value: 0 };
+
+  handleIncrement = () => {
+    this.setState((prevState) => {
+      return {
+        value: prevState.value + 1,
+      };
+    });
+  };
+  handleDicrement = () => {
+    this.setState((prevState) => {
+      return {
+        value: prevState.value - 1,
+      };
+    });
+  };
   render() {
     return (
       <div className="Counter">
-        <span className="value"></span>
+        <span className="value">{this.state.value}</span>
         <div className="controls">
-          <button type="button" className="plus"></button>
-          <button type="button" className="plus"></button>
+          <button type="button" className="btn" onClick={this.handleIncrement}>
+            +1
+          </button>
+          <button type="button" className="btn" onClick={this.handleDicrement}>
+            -1
+          </button>
         </div>
       </div>
     );
